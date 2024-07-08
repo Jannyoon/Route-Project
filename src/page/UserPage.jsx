c4fce79d1bafb50fc48fae4a82aa687c8c962d89
Jannyoon <5124193@naver.com>
2024-07-08 18:39:23 +0900

    (ADD)floating 메뉴 & 영역 밖 클릭 구현

--- src/page/UserPage.jsx
+++ src/page/UserPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useAuthContext } from '../context/useAuthContext';
 import useUserInfo from '../hook/useUserInfo';
 import { IoIosArrowBack } from "react-icons/io";
@@ -10,6 +10,12 @@ import { useNavigate } from 'react-router-dom';
 export default function UserPage() {
   const {user} = useAuthContext();
   const {userProfile} = useUserInfo();
+  const [view, setView] = useState(false);
+
+  const additionalView = useRef();
+  const threePoint = useRef();
+
+
   const navigate = useNavigate();
 
 
@@ -33,12 +39,50 @@ export default function UserPage() {
     navigate('/');
   }
 
+  //ref 영역 밖을 선택했을 때 Element가 보이지 않게...
+  useEffect(()=>{
+    console.log(view);
+    const handleClick =(e)=>{
+      if (threePoint.current){
+        console.log("포함하는가", threePoint.current.contains(e.target))
+        if (threePoint.current.contains(e.target)) return;
+        if (view && additionalView.current && !additionalView.current.contains(e.target)) setView(false);
+      }
+    }
+    document.addEventListener('click', handleClick);
+
+  }, [view])
+
+  const handleAdditionalView = ()=>{
+    setView(true);
+  }
+
   return (
     <div className='w-full flex flex-col items-center'>
       <div className='w-full flex items-center justify-between px-3 mt-6 pb-2 border-b'>
         <IoIosArrowBack className='text-xl md:text-3xl hover:cursor-pointer hover:text-brand' onClick={handleClick}/>
         <div className='text-xs md:text-lg'>{getProfile ? getProfile.nickName : "User"}</div>
-        <PiDotsThreeVerticalBold className='text-xl md:text-3xl hover:cursor-pointer hover:text-brand'/>
+        <div className='relative'>
+          <div ref={threePoint}><PiDotsThreeVerticalBold 
+            className='text-xl md:text-3xl hover:cursor-pointer hover:text-brand'
+            onClick={handleAdditionalView}
+          /></div>
+          {view && (
+            <div className='absolute 
+              flex flex-col text-center
+              top-2.5 right-2 w-24 p-1
+              lg:top-4 lg:right-3.5 lg:w-36
+            bg-red-300 hover:cursor-pointer'
+              ref={additionalView}
+            >
+              <div>1</div>
+              <div>2</div>
+              <div>3</div>
+              <div>4</div>
+              <div>5</div>
+          </div>)}        
+        </div>
+
       </div>
       <Profile info={userInfo}/>
       <div className='w-full flex gap-2 mt-7 md:mt-10'>
