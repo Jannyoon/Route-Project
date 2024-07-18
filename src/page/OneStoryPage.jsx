1931d4958c2cfe0e30ea1256e1f8244840b57182
Jannyoon <5124193@naver.com>
2024-07-18 13:04:03 +0900

    (MOD)스토리 카드 리팩토링

--- src/page/OneStoryPage.jsx
+++ src/page/OneStoryPage.jsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
-import StoryCard from '../component/StoryCard';
-import StoryCarousel from '../component/StoryCarousel';
+import StoryCardContents from '../component/StoryCardContents';
 
 export default function OneStoryPage() {
   const state = useLocation().state;
@@ -12,28 +11,10 @@ export default function OneStoryPage() {
       Story Page
     </div>
   );
-  const {contents, id, imgList, time, userId, userName, userProfileImg} = state;
-  
-
 
   return (
     <div className='w-full h-dvh mb-4 flex justify-center'>
-       <div className='w-full md:w-9/12 h-full border flex flex-col items-center'>
-          {/*스토리 캐러셀 */}
-          <StoryCarousel storyImgs={imgList}/>
-          <div 
-            className='flex-grow-0 w-full h-2/5 border-t
-            flex flex-col'
-          >
-            <div className='flex w-full items-center gap-2 m-3'>
-              <img src={userProfileImg} className='w-16 h-16 rounded-full'/>
-              <div>{userName}</div>
-              <button className='text-sm px-2 bg-brand rounded-md ml-2'>팔로우</button>
-            </div>
-            <div className='text-sm md:text-xs px-3 py-1'>이미지 개수 체크용: {imgList.length}</div>
-            <div className='text-sm md:text-xs px-3 py-1 overflow-y-auto'>{contents}</div>
-          </div>
-        </div>
+      <StoryCardContents data={state}/>
     </div>
   )
 
