9a272606a7a551c863e49625be9ccbd6f470c2a7
Jannyoon <5124193@naver.com>
2024-07-19 20:33:12 +0900

    (MOD)Story 페이지 코드 리팩토링

--- src/page/Story.jsx
+++ src/page/Story.jsx
@@ -5,24 +5,21 @@ import useServerStory from '../hook/useServerStory';
 import StoryCard from '../component/StoryCard';
 
 export default function Story() {
-  const storyRef = useRef();
   const { ref, inView } = useInView();
   const {getItems} = useServerStory();
-  const {data, status, error, fetchNextPage, isFetchingNextPage, hasNextPage} = getItems && getItems;
+  const {data, status, error, fetchNextPage, isFetchingNextPage, hasNextPage} = getItems;
   const [loadView, setLoadView] = useState(false);
 
-  console.log(data);//{pageParams, pages}
-  console.log("다음 페이지 존재 여부?", hasNextPage);
+  //console.log(data);//{pageParams, pages}
 
   const content = data && data.pages.map((pagedummy)=> pagedummy.map((obj,idx)=>{
+    //pagedummy.obj : [QuerySnapshot, {...스토리 정보}]
     let realData = obj[1];
-    //console.log("realdata", realData);
-    console.log("현재 idx", idx);
-    console.log("obj 길이", obj.length);
+   
+    //불러온 페이지 문서의 마지막 문서 => obj.length===(idx+1)
     if (obj.length===(idx+1)) return (<StoryCard innerRef={ref} data={realData}/>)
     return <StoryCard data={realData}/>
   }))
-
 
 
   useEffect(()=>{
@@ -35,15 +32,13 @@ export default function Story() {
       }, 1000);
     }
     return (()=>{time=undefined});
-
-    //해당 ref가 보여지는가 안 보여지는가의 여부인가요..?
   }, [inView, hasNextPage]);
   
 
 
   if (status && status==='error') return <div>Error: {error}</div>;
   return (
-    <div ref={storyRef} className='w-full h-dvh mb-4'>
+    <div className='w-full h-dvh mb-4'>
       {status==='pending' && <div>실행 중...</div>}
       <div className='w-full h-full'>
         {data && content}{' '}
