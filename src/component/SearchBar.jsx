009f59fd4064dcb9fa5e8b88afeb97dce8257c58
Jannyoon <5124193@naver.com>
2024-08-06 22:46:29 +0900

    (ADD)검색 자동완성 로직 구현

--- src/component/SearchBar.jsx
+++ src/component/SearchBar.jsx
@@ -7,24 +7,50 @@ export default function SearchBar() {
   const {snap, dataTrie} = useProductsContext();
   const [serverProducts, setServerProducts] = useState();
   const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(false);
+  const [userSearchList, setUserSearchList] = useState([]);
+
   const handleChange = (e)=>{
     setText(e.target.value);
   }
 
+  const handleFocusIn = ()=>{
+    setIsTyping(true);
+  }
 
+  const handleFocusOut = ()=>{
+    setIsTyping(false);
+  }
+
+  useEffect(()=>{
+    let searchList = dataTrie.search(text);
+    if (!searchList) setIsTyping(false);
+    else if (!isTyping && text!=='' && searchList) setIsTyping(true);
+    setUserSearchList(searchList || []);
+  },[text])
+
+  //test용
   useEffect(()=>{
     console.log("스냅 출력", snap);
     console.log(dataTrie);
   }, [snap])
 
-
+  console.log(text!=='' && dataTrie.search(text));
   return (
-    <form>
+    <form className='relative w-20 h-6 mr-1 md:w-60'>
       <input 
         className='w-20 h-6 mr-1 md:w-60 bg-transparent border-b-2 border-brand focus:outline-none focus:border-fcs p-2' placeholder='검색' 
         onChange={handleChange}
-        value={text}
-      />
+        onFocus={handleFocusIn}
+        onBlur={handleFocusOut}
+        value={text}/>
+
+      {isTyping && (<div className='absolute top-7 w-36 h-48 md:w-60 z-10 p-1 border bg-white flex flex-col justify-center items-center'>
+        <div className='w-full text-sm border-b pb-1 mb-1'>자동 완성 결과</div>
+        <div className='w-full h-full flex flex-col bg-slate-200 overflow-y-auto'>
+          {text==='' && '검색 결과가 없습니다'}
+        </div>
+      </div>)}
   </form>
   );
 }
--- src/hook/trieMaking.js
+++ src/hook/trieMaking.js
@@ -4,6 +4,7 @@ class Node{
     this.end=false;
     this.id='';
     this.child={};
+    this.data={};
   }
 }
 
@@ -13,7 +14,7 @@ class Trie {
     this.result = [];
   }
 
-  insert(string, id){
+  insert(string, id, data){
     //공백도 문자로 생각하고 작업한다.
     //공백 문자는 #로 child를 넣는다.
     let currentNode = this.root;
@@ -30,6 +31,7 @@ class Trie {
     }
     currentNode.end = true;
     currentNode.id = id;
+    currentNode.data = data;
   }
 
   search(string){
@@ -40,27 +42,41 @@ class Trie {
       if (currentNode.child[parameter]) currentNode = currentNode.child[parameter];
       else return false; //해당 문자열이 존재하지 않을 경우
     }
-    let list = this.getList(currentNode.child);
+    let list = this.getList(currentNode, currentNode.child);
+    list.sort((a,b)=>{
+      if (a[0]<b[0]) return -1;
+      return 1;
+    });
+
+    //개수 제한
+    if (list.length>10) return list.slice(0,11);
     return list;
     //끝까지 모두 찾았을 경우, 해당 child에 속한 식구들을 모두...
   }
 
-  getList(node){
+  getList(node, childObj){
     this.result = []; //초기화
-    this.dfs(node);
+    this.dfs(node, childObj);
     return this.result;
   }
 
-  dfs(node){
+  dfs(nowNode, childobj){
+    let nowValueStr = nowNode.value.split("").map(v =>{
+      if (v==='*') return " ";
+      else return v;
+    }).join("");
+
     //child가 없거나 단어가 이미 끝난 경우
-    if (Object.values(node.child).length===0){
-      this.result.push([node.value, node.id]);
+    if (Object.values(childobj).length===0){
+      this.result.push([nowValueStr, nowNode.id, nowNode.data]);
       return;
     }
-    for (let charNode of Object.entries(node.child)){
-      this.dfs(charNode);
+    for (let charNode of Object.entries(childobj)){
+      this.dfs(charNode[1], charNode[1].child);
     }    
-    if (node.end) this.result.push([node.value, node.id]);
+    if (nowNode.end){
+      this.result.push([nowValueStr, nowNode.id, nowNode.data]);
+    }
     return;
   }
 }
@@ -70,7 +86,7 @@ export default function searchTrie(database){
 
   for (let i=0; i<database.length; i++){
     const {productId, title} = database[i];
-    trie.insert(title, productId);
+    trie.insert(title, productId, database[i]);
   }
 
   return trie;
